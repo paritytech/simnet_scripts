26b9b62f1691b9037cf579da6874b52f9afdae46
radupopa2010 <radupopa2010@yahoo.com>
2021-03-17 18:04:37 +0100

    added gitlab runner and CI file

---
+++ .maintain/k8s-resources/gitlab/commands.sh
@@ -0,0 +1,43 @@
+# Add the repo
+https://docs.gitlab.com/runner/install/kubernetes.html#installing-gitlab-runner-using-the-helm-chart
+helm repo list
+helm repo add gitlab https://charts.gitlab.io
+helm repo update
+
+# Customizing the Chart Before Installing
+helm show values gitlab/gitlab-runner > values.yaml
+# edit the file
+
+# check current runer installation
+helm ls -n gitlab-simnetscripts-ci
+
+
+# create secret for  chache runner
+# docs example: https://docs.gitlab.com/runner/configuration/advanced-configuration.html#the-runnerscachegcs-section
+# get credentials from here https://console.developers.google.com/apis/credentials?project=parity-simnet
+kubectl -n gitlab-simnetscripts-ci  create secret generic gcsaccess     --from-literal=gcs-access-id="cache-for-runner@parity-simnet.iam.gserviceaccount.com"      --from-literal=gcs-private-key='"-----BEGIN PRIVATE KEY-----\nXXXXXX\n-----END PRIVATE KEY-----\n'
+
+helm install simnetscripts-gitlab-runner  \
+     gitlab/gitlab-runner \
+     -f values.yaml \
+     --namespace gitlab-simnetscripts-ci
+
+helm install simnetscripts-build-gitlab-runner  \
+     gitlab/gitlab-runner \
+     -f build-values.yaml \
+     --namespace gitlab-simnetscripts-ci
+
+# pull repo locally for inspection
+helm pull gitlab/gitlab-runner --untar
+
+# Push changes 
+helm upgrade simnetscripts-gitlab-runner gitlab/gitlab-runner  \
+     -f values.yaml \
+     --namespace gitlab-simnetscripts-ci
+
+
+
+helm upgrade simnetscripts-build-gitlab-runner gitlab/gitlab-runner  \
+     -f build-values.yaml \
+     --namespace gitlab-simnetscripts-ci
+
