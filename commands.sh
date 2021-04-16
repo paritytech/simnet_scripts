9a44ae21a9faf11060d7e52671edc5589ba6874e
radupopa2010 <radupopa2010@yahoo.com>
2021-04-16 20:59:41 +0200

    give absolute path for ci

--- commands.sh
+++ commands.sh
@@ -3,7 +3,7 @@
 # run local test of what pipelines runs
 docker run --volume /etc/gurke-container:/etc/gurke/  \
            docker.io/paritytech/simnetscripts:latest \
-               gurke/scripts/run-test-environment-manager.sh  \
+               /home/nonroot/gurke/scripts/run-test-environment-manager.sh  \
                   --test-script=../../simnet/testing/parachains/run_tests.sh \
                   --image="docker.io/paritypr/synth-wave:2714" \
                   --image-2="docker.io/paritypr/colander:2714"
